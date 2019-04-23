6a11171a96e9b208c92ed66e09950ce8be09a332
Samuel <samuelson_263@hotmail.com>
2019-04-23 22:28:41 +0200

    varable de entorno MONGO

--- server/config/congif.js
+++ server/config/congif.js
@@ -21,7 +21,19 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://skiperlay:fm1P6VpFYVPRcMf3@cafe-odf7e.mongodb.net/cafe';
+    /*
+    -para crear una variable de entorno en heroku:
+        heroku config set:MONGO_URI ="mongodb+srv://skiperlay:fm1P6VpFYVPRcMf3@cafe-odf7e.mongodb.net/cafe"
+        siento "MONGO_URI" el nombre de la variable que estamos creando
+    -para ver las variables de entorno de heroku:
+        heroku config
+    -para ver el valor de la variable de entorno "MONGO_URI" de heroku:
+        heroku config:get nombre
+    -para borrar la variable creada "MONGO_URI" en heroku:
+        heroku config:unset MONGO_URI
+    */
+    urlDB = process.env.MONGO_URI;
+    //urlDB = 'mongodb+srv://skiperlay:fm1P6VpFYVPRcMf3@cafe-odf7e.mongodb.net/cafe';
 }
 
 process.env.URLDB = urlDB;
